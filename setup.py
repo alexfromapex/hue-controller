87e65dfaa76994accbfe6c679173f0617548d5ca
Alex W <alexfromapex@gmail.com>
2019-10-10 17:45:30 -0400

    Setup.py fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='hue-controller',
@@ -9,7 +9,7 @@ setup(
     author_email='alexfromapex at gmail dot com',
     version='0.1dev',
     description='Control Phillips Hue lights with WX GUI',
-    packages=['wx','phue'],
+    install_requires=['wx','phue'],
     license='MIT',
     long_description=open('README.md').read(),
     url = 'https://github.com/alexfromapex/hue-controller'
